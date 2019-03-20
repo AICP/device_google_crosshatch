64cf5d9bf335e8dd7a3dade12f5e13170f659435
LorDClockaN <davor@losinj.com>
2019-03-20 19:54:55 +0100

    AICPfy
    Change-Id: I460deb0c0994e9652adf1ef3c434d076e75f9dfe

--- aicp_blueline.mk
+++ aicp_blueline.mk
@@ -2,16 +2,16 @@
 TARGET_SCREEN_HEIGHT := 2160
 TARGET_SCREEN_WIDTH := 1080
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common AICP stuff.
+$(call inherit-product, vendor/aicp/config/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/google/crosshatch/aosp_blueline.mk)
 
--include device/google/crosshatch/blueline/device-lineage.mk
+-include device/google/crosshatch/blueline/device-aicp.mk
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_NAME := lineage_blueline
+PRODUCT_NAME := aicp_blueline
 PRODUCT_BRAND := google
 PRODUCT_MODEL := Pixel 3
 TARGET_MANUFACTURER := Google
@@ -24,3 +24,4 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
 BUILD_FINGERPRINT := google/blueline/blueline:9/PQ2A.190305.002/5240760:user/release-keys
 
 $(call inherit-product-if-exists, vendor/google/crosshatch/crosshatch-vendor.mk)
+$(call inherit-product-if-exists, vendor/pixelgapps/pixel-gapps.mk)
