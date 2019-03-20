fced66f37e6ddcf1b5c8de2fd6306954363767ce
LorDClockaN <davor@losinj.com>
2019-03-20 19:54:55 +0100

    AICPfy
    Change-Id: I460deb0c0994e9652adf1ef3c434d076e75f9dfe

--- aicp_crosshatch.mk
+++ aicp_crosshatch.mk
@@ -2,20 +2,28 @@
 TARGET_SCREEN_HEIGHT := 2960
 TARGET_SCREEN_WIDTH := 1440
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common AICP stuff.
+$(call inherit-product, vendor/aicp/config/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/google/crosshatch/aosp_crosshatch.mk)
 
--include device/google/crosshatch/crosshatch/device-lineage.mk
+-include device/google/crosshatch/crosshatch/device-aicp.mk
+
+# Screen Resolution for the Bootanimation
+TARGET_SCREEN_HEIGHT := 2560
+TARGET_SCREEN_WIDTH := 1440
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_NAME := lineage_crosshatch
+PRODUCT_NAME := aicp_crosshatch
 PRODUCT_BRAND := google
 PRODUCT_MODEL := Pixel 3 XL
 TARGET_MANUFACTURER := Google
 PRODUCT_RESTRICT_VENDOR_FILES := false
+
+# AICP Device Maintainers
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    DEVICE_MAINTAINERS="Davor B (LorD ClockaN)"
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=crosshatch \
@@ -24,3 +32,4 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
 BUILD_FINGERPRINT := google/crosshatch/crosshatch:9/PQ2A.190305.002/5240760:user/release-keys
 
 $(call inherit-product-if-exists, vendor/google/crosshatch/crosshatch-vendor.mk)
+$(call inherit-product-if-exists, vendor/pixelgapps/pixel-gapps.mk)
